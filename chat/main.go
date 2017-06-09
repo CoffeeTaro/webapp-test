d52272c43ccf58e1bd3e3020f213ed5b0a64cbb3
Asamin <kotaro.a@uec.ac.jp>
2017-06-09 13:26:16 +0900

    use template

--- chat/main.go
+++ chat/main.go
@@ -2,21 +2,26 @@ package main
 
 import "log"
 import "net/http"
+import "sync"
+import "text/template"
+import "path/filepath"
+
+type templateHandler struct {
+	once     sync.Once
+	filename string
+	templ    *template.Template
+}
+
+func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	t.once.Do(func() {
+		t.templ = template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
+	})
+	t.templ.Execute(w, nil)
+}
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte(`
-			<html>
-				<head>
-					<title>チャット</title>
-				</head>
-				<body>
-					チャットしましょう
-				</body>
-			</html>
-		
-		`))
-	})
+	// route
+	http.Handle("/", &templateHandler{filename: "chat.html"})
 	// Start Web server
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal("ListenAndServe:", err)
